fee6404c06c66d92283677d17d1b536497348805
BrandonMefford <B.mefford1997@gmail.com>
2020-08-16 15:55:52 -0500

    added reservation model and context

---
+++ Nothing Fancy/Nothing Fancy/Models/DataContext/ReservationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nothing_Fancy.Models.DataContext
+{
+    public class ReservationDbContext : DbContext
+    {
+        public ReservationDbContext(DbContextOptions<ReservationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reservation> Reservation { get; set; }
+    }
+}
---
+++ Nothing Fancy/Nothing Fancy/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nothing_Fancy.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string reserverName { get; set; }
+        public string nameOfRoom { get; set; }
+        public DateTime reserveDate { get; set; }
+        public double cost { get; set; }
+    }
+}
