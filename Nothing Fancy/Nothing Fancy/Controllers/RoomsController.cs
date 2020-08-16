7c68fc2c551dfe234253ac710e483a40ba9d5447
unknown <kitsuneace@gmail.com>
2020-08-16 11:51:27 -0500

    Added Room Webpages

---
+++ Nothing Fancy/Nothing Fancy/Controllers/RoomsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nothing_Fancy.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room101()
+        {
+            return View();
+        }
+
+        public IActionResult Room102()
+        {
+            return View();
+        }
+
+        public IActionResult Room103()
+        {
+            return View();
+        }
+
+        public IActionResult Room104()
+        {
+            return View();
+        }
+    }
+}
