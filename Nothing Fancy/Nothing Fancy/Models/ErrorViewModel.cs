e9cdee5b3571c471ba5f8414ba69db8265d6a855
BrandonMefford <B.mefford1997@gmail.com>
2020-08-16 11:03:42 -0500

    initial commit

---
+++ Nothing Fancy/Nothing Fancy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nothing_Fancy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
