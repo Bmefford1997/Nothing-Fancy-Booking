489639a5fda651628e792d7d80b3dca15ea71e90
BrandonMefford <B.mefford1997@gmail.com>
2020-08-17 22:00:54 -0500

    prepping reservation and calendar controllers to navigate to respective pages

---
+++ Nothing Fancy/Nothing Fancy/Controllers/CalendarController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nothing_Fancy.Views.Calendar
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
