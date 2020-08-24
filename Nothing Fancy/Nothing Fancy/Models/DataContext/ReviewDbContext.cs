003d96ea43bbcb93a8019029a030dac55145d2ff
BrandonMefford <B.mefford1997@gmail.com>
2020-08-23 23:29:43 -0500

    created review model and relevent dbContext

---
+++ Nothing Fancy/Nothing Fancy/Models/DataContext/ReviewDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nothing_Fancy.Models.DataContext
+{
+    public class ReviewDbContext : DbContext
+    {
+        public ReviewDbContext(DbContextOptions<ReviewDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Review> Review { get; set; }
+    }
+    
+}
+
---
+++ Nothing Fancy/Nothing Fancy/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nothing_Fancy.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string reviewerName { get; set; }
+        public double reviewRate { get; set; }
+        public string reviewContent { get; set; }
+    }
+}
