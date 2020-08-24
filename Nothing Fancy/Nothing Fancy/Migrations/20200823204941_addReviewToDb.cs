dbec6c857dc6fefc5877671a0c851c01c6ea0e18
BrandonMefford <B.mefford1997@gmail.com>
2020-08-23 23:27:52 -0500

    added migration of review to Db

---
+++ Nothing Fancy/Nothing Fancy/Migrations/20200823204941_addReviewToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nothing_Fancy.Migrations
+{
+    public partial class addReviewToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    reviewerName = table.Column<string>(nullable: false),
+                    reviewRate = table.Column<double>(nullable: false),
+                    reviewContent = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
