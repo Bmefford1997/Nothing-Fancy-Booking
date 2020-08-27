64faab2e2217f7ba21bedc20208a07ad3dfd295d
BrandonMefford <B.mefford1997@gmail.com>
2020-08-26 21:07:40 -0500

    reservation cost formatted to currency, if this breaks things, this is sad

---
+++ Nothing Fancy/Nothing Fancy/Migrations/ReservationDb/20200827005437_EditFormat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nothing_Fancy.Migrations.ReservationDb
+{
+    public partial class EditFormat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
