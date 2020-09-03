8c1df3b6c5d96834561712412b0095a3259ab227
BrandonMefford <B.mefford1997@gmail.com>
2020-09-03 14:01:23 -0500

    cleaned up the controller and redirects for reservation pages. added datetimepicker files to replace datepicker for future update

--- Nothing Fancy/Nothing Fancy/Controllers/ReservationController.cs
+++ Nothing Fancy/Nothing Fancy/Controllers/ReservationController.cs
@@ -28,7 +28,7 @@ namespace Nothing_Fancy.Controllers
 
             if (!String.IsNullOrEmpty(searchString))
             {
-                reservations = reservations.Where(s => (s.reserverName.Contains(searchString) ||
+                reservations = reservations.Where(s => (s.reserverName.Contains(searchString) || s.nameOfRoom.Contains(searchString) ||
                                               s.Id.Equals(searchString)));
             }
 
@@ -110,61 +110,11 @@ namespace Nothing_Fancy.Controllers
             {
                 _context.Add(reservation);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Calendar));
             }
             return View(reservation);
         }
 
-        // GET: Reservation/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.Reservation.FindAsync(id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-            return View(reservation);
-        }
-
-        // POST: Reservation/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,reserverName,nameOfRoom,reserveDateBegin,reserveDateEnd,cost")] Reservation reservation)
-        {
-            if (id != reservation.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(reservation);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReservationExists(reservation.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(reservation);
-        }
 
         // GET: Reservation/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -192,7 +142,7 @@ namespace Nothing_Fancy.Controllers
             var reservation = await _context.Reservation.FindAsync(id);
             _context.Reservation.Remove(reservation);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Calendar));
         }
 
         private bool ReservationExists(int id)
@@ -200,7 +150,7 @@ namespace Nothing_Fancy.Controllers
             return _context.Reservation.Any(e => e.Id == id);
         }
 
-        public async Task<IActionResult> Weekly()
+        public async Task<IActionResult> Calendar()
         {
             var reservations = from r in _context.Reservation
                                select r;
